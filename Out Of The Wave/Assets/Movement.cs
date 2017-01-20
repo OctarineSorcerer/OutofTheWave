e22f043cdf6cec1948a50174c3a59d9b9812d7db
Gomme <dsdgom@essex.ac.uk>
2017-01-20 18:27:04 +0000

    Initial player (using temp wave sprite for now)

---
+++ Out Of The Wave/Assets/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Start() { }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Translate(0, 0, z);
+    }
+}
