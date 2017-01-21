cc12b3608545c30d8d82590d5a1b09ac2bd00573
Dan Gomme <daniel.gomme@yahoo.co.uk>
2017-01-21 13:28:25 +0000

    New scene with sprite following 3D tilted plane. "Player" should be a child of whatever's tilted, and rotate SpriteHolder towards the camera if you need to

--- Out Of The Wave/Assets/Movement.cs
+++ Out Of The Wave/Assets/Movement.cs
@@ -2,16 +2,40 @@
 
 public class Movement : MonoBehaviour
 {
+    public string xAxis;
+    public string yAxis;
     public float speed = 10.0f;
+    private Animator animator;
 
-    void Start() { }
+    // Use this for initialization
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        if (string.IsNullOrEmpty(xAxis))
+            xAxis = "Horizontal";
+        if (string.IsNullOrEmpty(yAxis))
+            yAxis = "Vertical";
+    }
+
 
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        float xMove = Input.GetAxis(xAxis);
+        float yMove = Input.GetAxis(yAxis);
+        var x = xMove * Time.deltaTime * speed;
+        var z = yMove * Time.deltaTime * speed;
 
         transform.Rotate(0, 0, 0);
-        transform.Translate(x, y, 0);
+        transform.Translate(x, 0, z); //Move the character
+
+        //This may throw up warnings if an animator is not attached and set up correctly
+        if (xMove > 0)                                     //change character's sprite because of key pressed
+            animator.SetTrigger("playerRight");
+        else if (xMove < 0)
+            animator.SetTrigger("playerLeft");
+        if (yMove > 0)
+            animator.SetTrigger("playerUp");
+        if (yMove < 0)
+            animator.SetTrigger("playerDown");
     }
 }
