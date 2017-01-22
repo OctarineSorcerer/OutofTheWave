40fff82c0abbd90b9e40773420a967e956b28bf6
Dan Gomme <daniel.gomme@yahoo.co.uk>
2017-01-22 01:32:05 +0000

    SO many changes. Aiming smooth with both controller/mouse Firing waves Waves now shouldn't have funky falling-off physics Just niceness

--- Out Of The Wave/Assets/AimingAndFire.cs
+++ Out Of The Wave/Assets/AimingAndFire.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class AimingAndFire : MonoBehaviour {
 
-    private CharacterController characterController;
     public int joystickNumber;
+    public float angle = 0;
+    public GameObject wave;
+    public Transform parentTransform;
+
+    private CharacterController characterController;
     private Animator animator;
 
     // Use this for initialization
     void Start () {
-        animator = gameObject.GetComponent<Animator>();
+        animator = gameObject.GetComponent<Animator>(); //For if you need to set firing
         characterController = GetComponent<CharacterController>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         string joystickString = joystickNumber.ToString();
-        float xAim = Input.GetAxis("RightJoystickX_P" + joystickString);
-        float yAim = Input.GetAxis("RightJoystickY_P" + joystickString);
-        float fire = Input.GetAxis("PrimaryFire_P" + joystickString); //axis for LT/RT, -1 to 0 is LT, 0 to 1 is RT
+        float rightX = Input.GetAxis("RightJoystickX_P" + joystickString);
+        float rightY = Input.GetAxis("RightJoystickY_P" + joystickString);
+        float fire = Input.GetAxis("PrimaryFire_P" + joystickString); //RT -1 to 0 for left, 0 to 1 for right
+
+        // We are going to read the input every frame
+        Vector3 vNewInput = new Vector3(rightX, rightY, 0.0f);
+
+        // Only for larger inputs
+	    if (vNewInput.sqrMagnitude >= 0.1f)
+	    {
+
+	        // Angle the stick is at
+	        angle = Mathf.Atan2(rightX, rightY)*Mathf.Rad2Deg;
+
+	        Vector3 eulers = transform.localEulerAngles; //Current angles
+
+	        //Silky smooth rotation for dat arrow keys satisfaction
+	        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(eulers.x, angle - 90, eulers.z), .15f);
+	    }
+
+	    // Fire a wave (controller) - Radu
+        if (fire > 0)
+        {
+            //Second param needs to be the position right in front of the arrow
+            GameObject waveInstance = Instantiate(wave, transform.position, transform.rotation, parentTransform);
+        }
     }
 }
---
+++ Out Of The Wave/Assets/FallWhenOff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class FallWhenOff : MonoBehaviour
+{
+    public Collider ToFall;
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("YOU DEAD");
+        ToFall.attachedRigidbody.useGravity = true;
+        ToFall.attachedRigidbody.isKinematic = false;
+    }
+}
